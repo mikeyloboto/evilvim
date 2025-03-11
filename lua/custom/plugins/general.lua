f55adad7ac8e85481a7852248ebf199ff42163cc
Evilbas <dimonloboto@gmail.com>
2025-03-12 00:26:20 +0800

    move to new config to ts closer

--- lua/custom/plugins/general.lua
+++ lua/custom/plugins/general.lua
@@ -2,9 +2,13 @@ local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
 local cmp = require 'cmp'
 return {
   require('nvim-ts-autotag').setup {
-    enable_close = true, -- Automatically close tags
-    enable_rename = true, -- Automatically rename tags
-    enable_close_on_slash = false, -- Disable auto-closing on trailing `</`
+    setup = function() end,
+    get_opts = {
+
+      enable_close = true, -- Automatically close tags
+      enable_rename = true, -- Automatically rename tags
+      enable_close_on_slash = false, -- Disable auto-closing on trailing `</`
+    },
   },
   require('nvim-autopairs').setup {
     check_ts = true, -- Use Tree-sitter for smarter pairing (optional)
