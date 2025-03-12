dfc5a847c9d01c725e6f24165c990caacf5cd927
Evilbas <dimonloboto@gmail.com>
2025-03-12 13:44:55 +0800

    add git stuff

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -14,4 +14,24 @@ return {
     date_format = '%m-%d-%Y %H:%M:%S', -- template for the date, check Date format section for more options
     virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
   },
+  {
+    'SuperBo/fugit2.nvim',
+    build = false,
+    opts = {
+      width = 100,
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+      {
+        'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
+        dependencies = { 'stevearc/dressing.nvim' },
+      },
+    },
+    cmd = { 'Fugit2', 'Fugit2Diff', 'Fugit2Graph' },
+    keys = {
+      { '<leader>F', mode = 'n', '<cmd>Fugit2<cr>' },
+    },
+  },
 }
