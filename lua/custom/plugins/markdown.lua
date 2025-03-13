920efdb04cdfd630f1167c0b423bef47f6394c41
Evilbas <dimonloboto@gmail.com>
2025-03-13 11:22:58 +0800

    markdown render

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  vim.keymap.set('n', '<leader>lm', ':RenderMarkdown toggle<CR>', { desc = 'Toggle [M]arkdown Rendering' }),
+}
