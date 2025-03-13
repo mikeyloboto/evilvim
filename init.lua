4f2d452e1a049dea60a69854c30f1653d6af3e1f
Evilbas <dimonloboto@gmail.com>
2025-03-13 11:03:04 +0800

    csv plugin + wk group names

--- init.lua
+++ init.lua
@@ -264,6 +264,8 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>e', group = 'Tre[e]', mode = { 'n' } },
+        { '<leader>l', group = '[L]anguage', mode = { 'n' } },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
