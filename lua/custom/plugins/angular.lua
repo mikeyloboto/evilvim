185d911606a0cb961ef6a46567afc2eca2f5b9a3
Evilbas <dimonloboto@gmail.com>
2026-02-06 14:28:19 +0800

    cleanup

--- lua/custom/plugins/angular.lua
+++ lua/custom/plugins/angular.lua
@@ -2,40 +2,40 @@ local null_ls = require 'null-ls'
 
 return {
 
-  require('lspconfig').angularls.setup {
-    cmd = {
-      'ngserver',
-      '--stdio',
-      '--tsProbeLocations',
-      '/home/evilbas/.local/lib/node_modules',
-      '--ngProbeLocations',
-      '/home/evilbas/.local/lib/node_modules',
-    },
-    on_attach = function(client, bufnr) end,
-    capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    root_dir = require('lspconfig').util.root_pattern('angular.json', '.git'),
-  },
-  require('tailwindcss-colorizer-cmp').setup {
-    color_square_width = 2,
-  },
-  require('cmp').setup {
-    sources = {
-      { name = 'nvim_lsp' },
-      { name = 'buffer' },
-      { name = 'path' },
-    },
-    formatting = {
-      format = require('tailwindcss-colorizer-cmp').formatter,
-    },
-  },
-  require('telescope').setup {
-    defaults = {
-      file_ignore_patterns = { 'node_modules' },
-    },
-  },
-  null_ls.setup {
-    sources = {
-      null_ls.builtins.formatting.prettier,
-    },
-  },
+  --  vim.lsp.config.angularls.setup {
+  --    cmd = {
+  --      'ngserver',
+  --      '--stdio',
+  --      '--tsProbeLocations',
+  --      '/home/evilbas/.local/lib/node_modules',
+  --      '--ngProbeLocations',
+  --      '/home/evilbas/.local/lib/node_modules',
+  --    },
+  --    on_attach = function(client, bufnr) end,
+  --    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  --    root_dir = vim.lsp.config.util.root_pattern('angular.json', '.git'),
+  --  },
+  --  require('tailwindcss-colorizer-cmp').setup {
+  --    color_square_width = 2,
+  --  },
+  --  require('cmp').setup {
+  --    sources = {
+  --      { name = 'nvim_lsp' },
+  --      { name = 'buffer' },
+  --      { name = 'path' },
+  --    },
+  --    formatting = {
+  --      format = require('tailwindcss-colorizer-cmp').formatter,
+  --    },
+  --  },
+  --  require('telescope').setup {
+  --    defaults = {
+  --      file_ignore_patterns = { 'node_modules' },
+  --    },
+  --  },
+  --  null_ls.setup {
+  --    sources = {
+  --      null_ls.builtins.formatting.prettier,
+  --    },
+  --  },
 }
