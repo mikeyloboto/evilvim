a3f23ea603b32a72c320797a13f38c6273f071fa
Evilbas <dimonloboto@gmail.com>
2026-02-06 14:56:19 +0800

    remove some keymaps

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -12,14 +12,4 @@ return {
     words = { enabled = true },
   },
   -- stylua: ignore
-  keys = {
-    { "<leader>n", function()
-      if Snacks.config.picker and Snacks.config.picker.enabled then
-        Snacks.picker.notifications()
-      else
-        Snacks.notifier.show_history()
-      end
-    end, desc = "Notification History" },
-    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-  },
 }
