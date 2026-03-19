f2c9303eae754c4d9ef25289d310903ef9ae4058
Evilbas <dimonloboto@gmail.com>
2026-03-19 11:42:47 +0800

    remove double autopair

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -11,6 +11,6 @@ return {
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    -- cmp.event:off('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
